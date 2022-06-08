51d093fb5e62280e303e9b94e9816aaa7538da42
Nabil Syahfiar <syahfiarnabil@gmail.com>
2022-06-08 13:06:17 +0700

    ubah variable product-categories

--- ajax/product-categories.php
+++ ajax/product-categories.php
@@ -4,14 +4,14 @@
 
    $category = $_GET['q'];
 
-   $sql = "SELECT coffees.id AS 'coffeeID', coffees.name, coffees.category, coffees.price, galleries.image
-            FROM coffees INNER JOIN galleries ON coffees.id = galleries.coffee_id
+   $sql = "SELECT products.id AS 'productID', products.name, products.category, products.price, galleries.image
+            FROM products INNER JOIN galleries ON products.id = galleries.product_id
             WHERE galleries.type = 'main'";
 
    if($category == 'all') { $sql .= ";"; }
-   else if($category == 'arabica') { $sql .= "AND coffees.category = 'arabica';"; }
-   else if($category == 'liberica') { $sql .= "AND coffees.category = 'liberica';"; }
-   else if($category == 'robusta') { $sql .= "AND coffees.category = 'robusta';"; }
+   else if($category == 'arabica') { $sql .= "AND products.category = 'arabica';"; }
+   else if($category == 'liberica') { $sql .= "AND products.category = 'liberica';"; }
+   else if($category == 'robusta') { $sql .= "AND products.category = 'robusta';"; }
 
    $result = mysqli_query($conn, $sql);
 
@@ -24,28 +24,28 @@
 <?php 
    if (mysqli_num_rows($result) > 0):
       // output data of each row
-      while($coffee = mysqli_fetch_assoc($result)):
+      while($product = mysqli_fetch_assoc($result)):
 ?>
 
 <div class="col-xl-3 col-md-4 col-sm-6 col-10 offset-1 offset-sm-0 card-container">
    <div class="product-card-container text-center">
       <div class="product-card pb-4">
          <div>
-            <img src="<?= $coffee['image']; ?>" alt="" class="product-image">
+            <img src="<?= $product['image']; ?>" alt="" class="product-image">
          </div>
          <div class="btn product-category mb-1">
-            <?= $coffee['category']; ?>
+            <?= $product['category']; ?>
          </div>
          <div class="product-name mb-1">
-            <a href="details.php?id=<?= $coffee['coffeeID']; ?>" class="stretched-link">
-               <?= $coffee['name']; ?>
+            <a href="details.php?id=<?= $product['productID']; ?>" class="stretched-link">
+               <?= $product['name']; ?>
             </a>
          </div>
          <div class="product-price">
-            <?= "Rp. " . number_format($coffee['price'],0,',','.'); ?>
+            <?= "Rp. " . number_format($product['price'],0,',','.'); ?>
          </div>
       </div>
-         <a href="details.php?id=<?= $coffee['coffeeID']; ?>" class="btn btn-cart">
+         <a href="details.php?id=<?= $product['productID']; ?>" class="btn btn-cart">
             <i class="fas fa-chevron-down"></i>
          </a>
    </div>
