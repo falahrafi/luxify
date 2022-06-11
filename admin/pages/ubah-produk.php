d46001cdc2227a06d4281c2f2aa8614f7e76771d
falahrafi <falahabdurrafi@gmail.com>
2022-06-11 21:19:23 +0700

    Melengkapi Gambar Produk

--- admin/pages/ubah-produk.php
+++ admin/pages/ubah-produk.php
@@ -1,26 +1,26 @@
-<?php 
+<?php
 
-   require_once '../../connection.php';
+require_once '../../connection.php';
 
-   $productID = $_GET['id'];
+$productID = $_GET['id'];
 
-   $sql = "SELECT * FROM products WHERE id = $productID";
-   $result = mysqli_query($conn, $sql);
+$sql = "SELECT * FROM products WHERE id = $productID";
+$result = mysqli_query($conn, $sql);
 
-   $rows = [];
-   while($row = mysqli_fetch_assoc($result)){
-      $rows[] = $row;
-   }
-   $coffee = $rows[0];
+$rows = [];
+while ($row = mysqli_fetch_assoc($result)) {
+    $rows[] = $row;
+}
+$product = $rows[0];
 
 ?>
 
 <!-- Page Heading -->
 <div class="d-sm-flex align-items-center justify-content-between mb-4">
-   <h1 class="h3 mb-0 text-dark">
-      <a href="index.php" class="btn-back" role="button"><i class="fas fa-long-arrow-alt-left"></i></a>
-      Ubah Produk
-   </h1>
+    <h1 class="h3 mb-0 text-dark">
+        <a href="index.php" class="btn-back" role="button"><i class="fas fa-long-arrow-alt-left"></i></a>
+        Ubah Produk
+    </h1>
 </div>
 
 <!-- Basic Card Example -->
@@ -29,14 +29,14 @@
         <h6 class="m-0 font-weight-bold text-white">Form Ubah Produk</h6>
     </div>
     <div class="card-body text-black">
-        
+
         <form action="functions/coffee/ubah.php?id=<?= $productID; ?>" method="POST">
             <div class="form-row">
                 <div class="form-group col">
                     <label for="inputName">
-                        <i class="fas fa-coffee fa-sm"></i>&ensp;<b>Nama</b>
+                        <i class="fas fa-air-freshener"></i>&ensp;<b>Nama</b>
                     </label>
-                    <input type="name" name="name" class="form-control" id="inputName" value="<?= $coffee['name']; ?>" required>
+                    <input type="name" name="name" class="form-control" id="inputName" value="<?= $product['name']; ?>" required>
                 </div>
             </div>
             <div class="form-row">
@@ -45,9 +45,8 @@
                         <i class="fas fa-tags fa-sm"></i>&ensp;<b>Kategori</b>
                     </label>
                     <select id="inputCategory" class="form-control" name="category" required>
-                        <option value="Arabica" <?= $coffee['category'] == 'Arabica' ? 'selected' : ''; ?>>Arabica</option>
-                        <option value="Liberica" <?= $coffee['category'] == 'Liberica' ? 'selected' : ''; ?>>Liberica</option>
-                        <option value="Robusta" <?= $coffee['category'] == 'Robusta' ? 'selected' : ''; ?>>Robusta</option>
+                        <option value="Men" <?= $product['category'] == 'Men' ? 'selected' : ''; ?>>For Men</option>
+                        <option value="Women" <?= $product['category'] == 'Women' ? 'selected' : ''; ?>>For Women</option>
                     </select>
                 </div>
                 <div class="form-group col-md-6">
@@ -58,19 +57,14 @@
                         <div class="input-group-prepend">
                             <div class="input-group-text">Rp.</div>
                         </div>
-                        <input type="number" name="price" class="form-control" id="inputPrice"  value="<?= $coffee['price']; ?>" step="100" required>
+                        <input type="number" name="price" class="form-control" id="inputPrice" value="<?= $product['price']; ?>" step="100" required>
                     </div>
                 </div>
                 <div class="form-group col">
                     <label for="inputDescription">
                         <i class="fas fa-align-left fa-sm"></i>&ensp;<b>Deskripsi</b>
                     </label>
-                    <textarea
-                        class="form-control"
-                        id="inputDescription" 
-                        name="description" 
-                        rows="6"
-                     ><?= $coffee['description']; ?></textarea>
+                    <textarea class="form-control" id="inputDescription" name="description" rows="6"><?= $product['description']; ?></textarea>
                 </div>
             </div>
             <button type="submit" class="btn btn-black">
--- connection.php
+++ connection.php
@@ -2,7 +2,7 @@
 
 $servername = "localhost";
 $username = "root";
-$password = "root";
+$password = "";
 $dbname = "luxify";
 
 // Create connection
