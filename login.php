59b8517d00e02835aeb02e07fa5c218234e8ebc7
falahrafi <falahabdurrafi@gmail.com>
2022-06-17 01:11:47 +0700

    Merapikan Login Page

--- login.php
+++ login.php
@@ -20,54 +20,94 @@
 
 <body>
 
-<!-- Ellipse Elements -->
-<div class="container ellipse-container">
-  <img src="assets/images/ellipse-b-l.png" class="ellipse-element ellipse-b-l">
-  <img src="assets/images/ellipse-b-r.png" class="ellipse-element ellipse-b-r">
-  <img src="assets/images/ellipse-b-t.png" class="ellipse-element ellipse-b-t">
-</div>
+  <!-- Reversed - Ellipse Elements -->
+  <div class="container ellipse-container">
+    <img src="assets/images/ellipse-b-l.png" class="ellipse-element ellipse-b-l">
+    <img src="assets/images/ellipse-b-r.png" class="ellipse-element ellipse-b-r">
+    <img src="assets/images/ellipse-b-t.png" class="ellipse-element ellipse-b-t">
+  </div>
 
-<section class="contact">
-  <div class="container">
-    <div class="row contact-card justify-content-center">
-      <div class="col-lg-4 col-sm-10 col-11 offset-lg-1">
-        <form action="auth.php" method="POST" class="row">
-          <div class="col-12 mb-5">
-            <img src="assets/images/luxify-logo-text.png" alt="" height="44">
-          </div>
-          <div class="col-12">
-            <img src="assets/images/illustration-contact.png" alt="Ilustrasi Kontak" class="img-fluid d-lg-none">
-          </div>
-          <div class="col-12">
-            <label for="inputNama" class="form-label">Username</label>
-            <input type="text" name="user" class="form-control contact-input" id="inputNama" required>
-          </div>
-          <div class="col-12">
-            <label for="inputEmail" class="form-label">Password</label>
-            <input type="password" name="pass" class="form-control contact-input" id="inputEmail" required>
-          </div>
-          <div class="col-12">
-            <button type="submit" class="btn btn-kirim-pesan px-4 float-end">
-              Login
-            </button>
-          </div>
-          <div class="col-12">
-            <h5 class="float-end mt-3">
-              Tidak punya akun? <a href="#" class="text-decoration-none">Daftar sekarang</a>
-            </h5>
-          </div>
-        </form>
-      </div>
+  <section class="contact">
+    <div class="container">
+      <div class="row contact-card justify-content-center">
+        <!-- Illustration: LG - XL -->
+        <div class="col-lg-6 col-sm-10 col-11 text-center">
+          <img src="assets/images/illustration-login.png" alt="Ilustrasi Kontak" class="img-fluid contact-illustration d-none d-lg-block">
+        </div>
 
-      <div class="col-lg-7 col-sm-10 col-11 text-center">
-        <img src="assets/images/access-control-system-abstract-concept.webp" alt="Ilustrasi Kontak" class="img-fluid contact-illustration d-none d-lg-block">
+        <div class="col-lg-4 col-sm-10 col-11">
+          <form action="auth.php" method="POST" class="row">
+
+            <div class="col-12 text-center mb-5">
+              <!-- Logo: LG - XL -->
+              <img src="assets/images/luxify-logo.png" alt="" height="100" class="d-none d-lg-block mx-auto mb-4">
+              <img src="assets/images/luxify-text-dark.png" alt="" height="40" class="d-none d-lg-block mx-auto">
+              <!-- Logo: XS - MD -->
+              <img src="assets/images/luxify-logo-text.png" alt="" height="56" class="d-lg-none mx-auto">
+            </div>
+
+            <!-- Illustration: XS - MD -->
+            <div class="col-12">
+              <img src="assets/images/illustration-login.png" alt="Ilustrasi Kontak" class="img-fluid d-lg-none my-5">
+            </div>
+
+            <div class="col-12">
+              <label for="inputUsername" class="form-label">Username</label>
+              <div class="login-input-container">
+                <input type="text" name="user" class="form-control login-input" id="inputUsername" required>
+              </div>
+            </div>
+            <div class="col-12">
+              <label for="inputPassword" class="form-label">Password</label>
+              <div class="login-input-container">
+                <input type="password" name="pass" class="form-control login-input" id="inputPassword" required>
+                <button class="btn password-visibility float-end" type="button" onclick="passwordVisibility()">
+                  <i class="fas fa-eye" id="iconVisibilityPassword"></i>
+                </button>
+              </div>
+            </div>
+
+            <div class="col-12">
+              <button type="submit" class="btn btn-login px-4 float-end">
+                Login
+              </button>
+            </div>
+            <div class="col-12 text-center">
+              <h5 class="mt-4">
+                Belum punya akun?
+              </h5>
+            </div>
+            <div class="col-12">
+              <button type="submit" class="btn btn-daftar-sekarang px-4">
+                Daftar Sekarang
+              </button>
+            </div>
+          </form>
+        </div>
       </div>
     </div>
-  </div>
-</section>
+  </section>
 
-<!-- Bootstrap javascript -->
-<script src="libraries/bootstrap-5.1.3-dist/js/bootstrap.bundle.js"></script>
+  <!-- Bootstrap javascript -->
+  <script src="libraries/bootstrap-5.1.3-dist/js/bootstrap.bundle.js"></script>
+  <script>
+    function passwordVisibility() {
+      var pwText = document.getElementById("inputPassword");
+      var pwVisibilityIcon = document.getElementById("iconVisibilityPassword");
+
+      if (pwText.type === "password") {
+        pwText.type = "text";
+        pwVisibilityIcon.classList.remove("fa-eye");
+        pwVisibilityIcon.classList.add("fa-eye-slash");
+        pwVisibilityIcon.style.color = "#00AEEF";
+      } else {
+        pwText.type = "password";
+        pwVisibilityIcon.classList.remove("fa-eye-slash");
+        pwVisibilityIcon.classList.add("fa-eye");
+        pwVisibilityIcon.style.color = "#1D1D1D";
+      }
+    }
+  </script>
 </body>
 
 </html>
