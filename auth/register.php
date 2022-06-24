103de54dd0424292dd637b600cd8172a048fbf89
falahrafi <falahabdurrafi@gmail.com>
2022-06-24 17:05:06 +0700

    Logic Improvement: Menambahkan kondisi jika username sudah ada pada database saat mendaftar akun

--- auth/register.php
+++ auth/register.php
@@ -7,16 +7,33 @@
     $usern = htmlspecialchars($_POST["user"]);
     $passw = md5($_POST["pass"]);
 
-    $query = "INSERT INTO user (name, usern, passw, level)
-                VALUES ('$name', '$usern', '$passw', 'user')";
+    // Apakah terdapat username yang sama pada database?
+    $usernameExists = "SELECT EXISTS (SELECT * FROM `user` WHERE usern = '$usern') AS 'exists';";
+    $resultExists = mysqli_query($conn, $usernameExists);
+    $rowExists = mysqli_fetch_assoc($resultExists);
 
-    mysqli_query($conn, $query);
-
-    echo "
-            <script>
-               alert('Berhasil membuat akun!');
-               document.location.href = '../index.php';
-            </script>
+    // Jika YA, jangan menambahkan user baru, redirect ke halaman login
+    if ($rowExists['exists'] == '1') {
+        echo "
+        <script>
+        alert('Username yang anda masukkan sudah terdaftar pada sistem kami, Silahkan Login!');
+        document.location.href = '../index.php';
+        </script>
         ";
+    } else {
+        // Jika TIDAK, lanjutkan menambahkan user baru
+
+        $query = "INSERT INTO user (name, usern, passw, level)
+                    VALUES ('$name', '$usern', '$passw', 'user')";
+
+        mysqli_query($conn, $query);
+
+        echo "
+                <script>
+                alert('Berhasil membuat akun!');
+                document.location.href = '../index.php';
+                </script>
+            ";
+    }
 
 ?>
